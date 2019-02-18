dc5bd3f8ff6484bba3a51160b7769b41a75bda7a
Tiago Montes <tiago.montes@promptar.com>
2019-02-18 12:50:10 +0000

    Fix QLocale test mock, adjusting PyQt5.Qt to PyQt5.QtCore.

--- tests/test_qtql.py
+++ tests/test_qtql.py
@@ -19,7 +19,7 @@ def test_short_name_for_es_MX_is_ES():
     # Mocking Qt call.
     qlocale_system = mock.Mock()
     qlocale_system.name.return_value = 'es_MX'
-    with mock.patch('PyQt5.Qt.QLocale.system', return_value=qlocale_system):
+    with mock.patch('PyQt5.QtCore.QLocale.system', return_value=qlocale_system):
         shortname = qtql.shortname()
 
     assert shortname == 'ES'
